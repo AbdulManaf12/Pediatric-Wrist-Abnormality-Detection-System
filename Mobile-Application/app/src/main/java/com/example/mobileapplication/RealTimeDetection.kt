326da4360251fe9f56041c7e1b01647602f2a148
AbdulManaf12 <sahitoa97@gmail.com>
2023-03-02 13:30:35 +0500

    interface

--- Mobile-Application/app/src/main/java/com/example/mobileapplication/RealTimeDetection.kt
+++ Mobile-Application/app/src/main/java/com/example/mobileapplication/RealTimeDetection.kt
@@ -3,9 +3,10 @@ package com.example.mobileapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RealTimeDetection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_real_time_detection)
     }
+    
 }
