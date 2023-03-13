1e9ac6305e47424e1d858fa7bbb6c3a3058e826b
AbdulManaf12 <sahitoa97@gmail.com>
2023-03-13 18:53:49 +0500

    -

--- Mobile-Application/app/src/main/java/com/example/mobileapplication/DicomToPNG.kt
+++ Mobile-Application/app/src/main/java/com/example/mobileapplication/DicomToPNG.kt
@@ -1,16 +1,40 @@
 package com.example.mobileapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class DicomToPNG : AppCompatActivity() {
+
+    val PICK_IMAGE_REQUEST = 47
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         supportActionBar?.hide()
         setContentView(R.layout.activity_dicomtopng)
     }
+    fun selectImage(view : View){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "application/dicom"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            val uri: Uri? = data?.data
+        }
+    }
+
 }
